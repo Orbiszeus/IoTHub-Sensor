edb662804d349acb85eac30f2a947f3af0ffe29b
barisozdizdar <simonbateman90@gmail.com>
2024-05-15 14:55:49 +0200

    New client Json added and addition of connection of the new dataset.

--- controller/main.py
+++ controller/main.py
@@ -1,6 +1,6 @@
 from google.cloud import bigquery
 
-client = bigquery.Client.from_service_account_json('/Users/orbiszeus/Downloads/linear-aviary-420617-4ec57e3a8c4e.json')
+client = bigquery.Client.from_service_account_json('/Users/orbiszeus/Downloads/IOT_Final_KEY.json')
 
 # dataset_id = 'linear-aviary-420617.iotDB'
 # table_id = 'linear-aviary-420617.iotDB.Flight_Data'
@@ -10,43 +10,49 @@ client = bigquery.Client.from_service_account_json('/Users/orbiszeus/Downloads/l
 
 # Perform a query.
 QUERY = (
-    'SELECT ST_MAKELINE(ST_GEOGPOINT(longitude1, latitude1), ST_GEOGPOINT(longitude2, latitude2)) AS line, '
-    'longitude1 AS start_longitude, '
-    'latitude1 AS start_latitude, '
-    'longitude2 AS end_longitude, '
-    'latitude2 AS end_latitude, '
-    'call_sign AS call_sign '
-    'FROM `linear-aviary-420617.iotDB.Flight_Data` '
-    'WHERE velocity >= 60 '
-    'LIMIT 100'
+    'SELECT * FROM local-incline-419216.finalproject.finalproject LIMIT 100'
 )
 query_job = client.query(QUERY)  # API request
 
 rows = query_job.result()  # Waits for query to finish
-
+counter = 0
 for row in rows:
     # Accessing elements by index
-    line = row[0]
-    start_longitude = row[1]
-    start_latitude = row[2]
-    end_longitude = row[3]
-    end_latitude = row[4]
-    call_sign = row[5]
+    temperature = row[0]
+    humidity = row[1]
+    co = row[2]
+    alcohol = row[3]
+    co2 = row[4]
+    tolueno = row[5]
+    nh4 = row[6]
+    acetona = row[7]
+    date = row[8]
+    id = row[9]   
 
     # Accessing elements by field names from the dictionary
-    line = row['line']
-    start_longitude = row['start_longitude']
-    start_latitude = row['start_latitude']
-    end_longitude = row['end_longitude']
-    end_latitude = row['end_latitude']
-    call_sign = row['call_sign']
+    temperature = row['temperature']
+    humidity = row['humidity']
+    co = row['co']
+    alcohol = row['alcohol']
+    co2 = row['co2']
+    tolueno = row['Tolueno']
+    nh4 = row['NH4']
+    acetona = row['Acetona']
+    date = row['when']
+    id = row['ID_PLACA']
 
     # Print the values
-    print("Line:", line)
-    print("Start Longitude:", start_longitude)
-    print("Start Latitude:", start_latitude)
-    print("End Longitude:", end_longitude)
-    print("End Latitude:", end_latitude)
-    print("Call Sign:", call_sign)
-
+    
+    print("Temperature: ", temperature)
+    print("Humidity: ", humidity)
+    print("Carbon Monoxide: ", co)
+    print("Carbon Dioxide: ", co2)
+    print("Tolueno: ", tolueno)
+    print("Amoniac: ", nh4)
+    print("Acetone: ", acetona)
+    print("Date: ", date)
+    print("ID: ", id)
+    counter += 1 
+    print("Row: " ,counter)
+    print("-----------------------------------")
         
